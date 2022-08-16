57a4b5cb9da80fb092383c223bf541621600b65b
anubhav.d.garg <anubhavg18@gmail.com>
2022-08-16 14:57:54 +0530

    Added api key in network file

--- lib/network/network-service.ts
+++ lib/network/network-service.ts
@@ -1,10 +1,11 @@
 import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
 import { lastValueFrom, map } from "rxjs";
+import { ConfigService } from "lib/utils/config.service";
 
 @Injectable()
 export class NetworkService {
-  constructor(private readonly httpService: HttpService) { }
+  constructor(private readonly httpService: HttpService, private readonly configservice: ConfigService) { }
   // It will call the all request from 3rd party apis
 
   async get(url: string) {
@@ -16,7 +17,8 @@ export class NetworkService {
   async post<Type>(url: string, data: any, headers: any): Promise<Type> {
     let headersObject = {
       'Authorization': headers.authorization,
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': this.configservice.getConfigConstantValue('baseUrlKey')
     }
     const observable = await this.httpService.post(url, JSON.stringify(data), { headers: headersObject }).pipe(map((res) => res.data));
     // you can use the data object now !!
